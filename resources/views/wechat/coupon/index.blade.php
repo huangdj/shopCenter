a6525ea4d442301d0dfeac3e1b44f95df38ee6e5
huangdj <244500972@qq.com>
2021-03-01 08:33:03 +0800

    修改首页优惠券显示问题

--- resources/views/wechat/coupon/index.blade.php
+++ resources/views/wechat/coupon/index.blade.php
@@ -11,7 +11,7 @@
     <title>优惠券列表</title>
     <link type="text/css" rel="stylesheet" href="/vendor/wechat/css/style.css"/>
     <style>
-        .a2{
+        .a2 {
             color: #ccc;
             background: #ccc;
         }
@@ -19,31 +19,30 @@
 </head>
 
 <body>
-@if($coupons)
-    <div class="quannewsbox1">
-        @foreach($coupons as $coupon)
-            <div class="quannews_1" data-id="{{ $coupon->id }}">
-                <div class="quanbg"><img src="/vendor/wechat/images/q1.png"/></div>
-                <div class="quanb">
-                    <div class="quanL">
-                        <p class="p1">{{ $coupon->name }}</p>
-                        <p class="p2">{{ $coupon->not_before->format('Y年m月d日') }}至{{ $coupon->not_after->format('Y年m月d日') }}</p>
-                    </div>
-                    <div class="quanR">
-                        <p class="p3">￥{{ $coupon->value }}元</p>
-                        @if($coupon->recived == $coupon->total)
-                            <a href="javascript:;" class="a2 receive_code">已被抢光</a>
-                        @else
-                            <a href="javascript:;" class="a1 receive_code">立即领取</a>
-                        @endif
-                    </div>
-                    <div class="clear"></div>
+<div class="quannewsbox1" @if($coupons->isEmpty()) style="display: none" @endif>
+    @foreach($coupons as $coupon)
+        <div class="quannews_1" data-id="{{ $coupon->id }}">
+            <div class="quanbg"><img src="/vendor/wechat/images/q1.png"/></div>
+            <div class="quanb">
+                <div class="quanL">
+                    <p class="p1">{{ $coupon->name }}</p>
+                    <p class="p2">{{ $coupon->not_before->format('Y年m月d日') }}
+                        至{{ $coupon->not_after->format('Y年m月d日') }}</p>
                 </div>
-                <div class="quanbg"><img src="/vendor/wechat/images/q3.png"/></div>
-                <div class="quanstate"><img src="/vendor/wechat/images/q5.png"/></div>
+                <div class="quanR">
+                    <p class="p3">￥{{ $coupon->value }}元</p>
+                    @if($coupon->recived == $coupon->total)
+                        <a href="javascript:;" class="a2 receive_code">已被抢光</a>
+                    @else
+                        <a href="javascript:;" class="a1 receive_code">立即领取</a>
+                    @endif
+                </div>
+                <div class="clear"></div>
             </div>
-        @endforeach
-    </div>
+            <div class="quanbg"><img src="/vendor/wechat/images/q3.png"/></div>
+            <div class="quanstate"><img src="/vendor/wechat/images/q5.png"/></div>
+        </div>
+    @endforeach
     <div class="clear"></div>
     <div class="quannewsbox2">
         <div class="kbox"></div>
@@ -52,19 +51,19 @@
             <p class="p3">{{ $receive }}</p>
         </div>
     </div>
-@else
-    <div class="quannewsbox1">
-        <div class="quannews_2 bg">
-            <div class="d1"><p>本商城暂未发放优惠券</p></div>
-            <div class="d2">
-                <div class="d21"><img src="/vendor/wechat/images/q13.png"/></div>
-                <div class="d22" style="color: #08c">暂无优惠活动，请持续关注~</div>
-                <div class="d21"><img src="/vendor/wechat/images/q13.png"/></div>
-                <div class="clear"></div>
-            </div>
+</div>
+
+<div class="quannewsbox1" @if(!$coupons->isEmpty()) style="display: none" @endif>
+    <div class="quannews_2 bg">
+        <div class="d1"><p>本商城暂未发放优惠券</p></div>
+        <div class="d2">
+            <div class="d21"><img src="/vendor/wechat/images/q13.png"/></div>
+            <div class="d22" style="color: #08c">暂无优惠活动，请持续关注~</div>
+            <div class="d21"><img src="/vendor/wechat/images/q13.png"/></div>
+            <div class="clear"></div>
         </div>
     </div>
-@endif
+</div>
 @include('layouts.wechat.shared._footer')
 
 <script type="text/javascript" src="/vendor/wechat/js/jquery.min.js"></script>
@@ -80,10 +79,10 @@
                 success: function (data) {
                     if (data.status = true) {
                         alert(data.message)
-                        location.href=location.href
+                        location.href = location.href
                     } else {
                         alert(data.message)
-                        location.href=location.href
+                        location.href = location.href
                     }
                 }
             })
